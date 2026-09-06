c12755632db8675a3333b66acfd74bba9418322a
Choudary Hussain Ali <hivenexis@gmail.com>
2026-09-07 01:26:28 +0500

    feat(config): read supabase credentials

--- config.py
+++ config.py
@@ -5,3 +5,5 @@ from dotenv import load_dotenv
 load_dotenv()
 
 FLASK_SECRET_KEY = os.environ.get("FLASK_SECRET_KEY", "")
+SUPABASE_URL = os.environ.get("SUPABASE_URL", "")
+SUPABASE_KEY = os.environ.get("SUPABASE_KEY", "")
